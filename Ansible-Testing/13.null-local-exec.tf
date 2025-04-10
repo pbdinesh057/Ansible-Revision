668bb53a9f915728e1ab2418daedf23c64706f6c
unknown <pbdinesh057@gmail.com>
2025-04-10 13:55:43 +0530

    made change to null resource

--- Ansible-Testing/13.null-local-exec.tf
+++ Ansible-Testing/13.null-local-exec.tf
@@ -3,6 +3,10 @@ resource "null_resource" "webservers" {
     command = <<EOH
       sleep 10
       ANSIBLE_HOST_KEY_CHECKING=False ansible -i invfile pvt -m ping
+      sleep 10
+      ANSIBLE_HOST_KEY_CHECKING=False ansible -i invfile pub -m ping
+      sleep 10
+      ANSIBLE_HOST_KEY_CHECKING=False ansible all -i invfile -m ping
     EOH
     environment = {
       ANSIBLE_HOST_KEY_CHECKING = "False"
