6f1bd4782efed7b4c378c030572feda534290989
unknown <pbdinesh057@gmail.com>
2025-04-10 13:53:18 +0530

    made changes to few vars

--- Ansible-Testing/13.null-local-exec.tf
+++ Ansible-Testing/13.null-local-exec.tf
@@ -2,9 +2,11 @@ resource "null_resource" "webservers" {
   provisioner "local-exec" {
     command = <<EOH
       sleep 10
-      ansible -i invfile pvt -m ping
+      ANSIBLE_HOST_KEY_CHECKING=False ansible -i invfile pvt -m ping
     EOH
+    environment = {
+      ANSIBLE_HOST_KEY_CHECKING = "False"
+    }
   }
   depends_on = [local_file.ansible-inventory-file]
 }
-
